8974be77b520ddf0a4923fe43bbee71a03daaeab
EC2 Default User <ec2-user@ip-172-31-36-128.us-east-2.compute.internal>
2019-03-19 22:20:50 +0000

    cucumber76.68 rspec87

--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -24,6 +24,24 @@ describe Admin::CategoriesController do
     assert_response :redirect, :action => 'index'
   end
 
+  describe "test_edit" do
+    before(:each) do
+      get :edit, :id => Factory(:category).id
+    end
+
+    it 'should render template new' do
+      assert_template 'new'
+      assert_tag :tag => "table",
+        :attributes => { :id => "category_container" }
+    end
+
+    it 'should have valid category' do
+      assigns(:category).should_not be_nil
+      assert assigns(:category).valid?
+      assigns(:categories).should_not be_nil
+    end
+  end
+  
   it "test_update" do
     post :edit, :id => Factory(:category).id
     assert_response :redirect, :action => 'index'
